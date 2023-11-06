bed50a40d728afadc4a0c9dfd0b43980f2ce9b90
nithish <nitishkumar.a@fcsonline.co.in>
2023-11-06 13:30:37 +0530

    changes

--- medical/medic/main.go
+++ medical/medic/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	api "medic/Api"
 	dash "medic/Dash"
@@ -10,6 +12,12 @@ import (
 
 func main() {
 	log.Println("Api Server started...")
+	f, err := os.OpenFile("./log/logfile"+time.Now().Format("02012006.15.04.05.000000000")+".txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
 
 	http.HandleFunc("/checkValid", api.CheckLog)
 
